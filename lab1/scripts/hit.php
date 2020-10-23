d91a218137bc4574e791be4f14d5051b38f74c61
damirJann <pilacis@list.ru>
2020-10-23 17:14:05 +0300

    Сделал всё. Буду рефакторить

--- lab1/scripts/hit.php
+++ lab1/scripts/hit.php
@@ -1,11 +1,55 @@
-<html>
-<head>
-    <title>Веб-сайт</title>
-</head>
-<body>
 <?php
-echo "<p>Привет мир!</p>";
-echo "2 + 2 = " . (2+2);
+function isValid($X, $Y, $R)
+{
+    $xRange = [];
+    $rRange = [];
+    $X = floatval($X);
+    $Y = floatval($Y);
+    $R = floatval($R);
+    for ($i = -5; $i <= 3; $i++) array_push($xRange, $i);
+    for ($i = 1; $i <= 3; $i+=0.5) array_push($rRange, $i);
+
+    if (isset($X) and isset($Y) and isset($R) and
+        in_array($X, $xRange) and ($Y <= 5) and ($Y >= -3) and in_array($R, $rRange)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function isInArea($X, $Y, $R)
+{
+
+    $X = floatval($X);
+    $Y = floatval($Y);
+    $R = floatval($R);
+
+    if ($X <= 0 and $X >= -$R and $Y <= 0 and $Y >= -$R) return true;
+    if ($X <= 0 and $Y <= 0 and $Y >= -$X / 2 - $R / 2) return true;
+    if ($X <= 0 and $Y >= 0 and $R >= $X * $X + $Y * $Y) return true;
+    return false;
+
+}
+
+
+$X = $_REQUEST['X'];
+$Y = $_REQUEST['Y'];
+$R = $_REQUEST['R'];
+
+
+if (isValid($X, $Y, $R) and isInArea($X, $Y, $R)){
+    $isHit = "Yes";
+}
+else{
+    $isHit = "NO";
+}
+
+echo "<tr>
+        <td>$X</td> 
+        <td>$Y</td>
+        <td>$R</td>
+        <td>$isHit</td>
+      </tr>"
 ?>
-</body>
-</html>
+
--- lab1/scripts/validation.js
+++ lab1/scripts/validation.js
@@ -1,37 +1,93 @@
-const rangeY = [-3 , -2, -1, 0, 1, 2, 3, 4, 5];
+class userTable{
+    data = {};
 
-document.getElementById("data").addEventListener("submit", function (event){
-    // validateForm(document.getElementsByTagName("input")[0]);
-    validateForm(event);
-});
+    getJSON(){
+        return this.data.stringify();
+    }
+
+    drop(){
+
+    }
+
+    addRecord(){
+
+    }
 
 
+}
+
+table = userTable
+const rangeX = [-5, -4,-3 , -2, -1, 0, 1, 2, 3, 4];
+const rangeR = [1, 1.5, 2, 2.5, 3];
+
+function dropTable(){
+    // let nodes = $("#result tr");
+    // for (let i = nodes.length-1; i > 0; i--){
+    //     nodes[i].remove();
+    //
+    // }
+}
+
+function updateTableCookie(newRecordJSON){
+    recordsJson = JSON.parse(getCookie(table));
+
+    recordsJson.add(newRecordJSON);
+
+    document.cookie += `table=${JSON.stringify(recordsJson)}`
+}
+
+function clearTableCookie(key, value){
+    document += `${key}=${value}`;
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 function validateForm(event){
+    event.preventDefault();
     let form = event.target;
     let input = form.elements['Y'];
 
-    if (!input){
-        input.setAttribute("class", "invalid");
+    if (!isYValid()){
+        makeWarn();
     }
-    else{
-        let data = [form.elements['X'], form.elements['Y'], form.elements['R']];
-        $.ajax({
-            url:'http://localhost:63342/lab1/scripts/hit.php?_ijt=tnu33npsl884v43qs1b6eoo87u',
-            data:{data},
-            success: function(isInArea){
-                if (isInArea === true){
-                    addToTable(data);
-                }
-            },
-            type: 'GET'
+    else {
+        let data = {"X":form.elements['X'].value,
+                    "Y": form.elements['Y'].value,
+                    "R":form.elements['R'].value
+        };
+        let startTime = new Date();
+
+        let response = $.get({
+            url: '..//scripts//hit.php',
+            data: data,
+        });
+        response.done(function (response){
+            let scriptTime = endTime.getTime() - new Date().getTime();
+
+            // добавляем данные о времени в поле
+            responseJson = JSON.parse(response);
+            responseJson["Время выполнения"] = scriptTime;
+            responseJson["Время запроса"] = startTime;
+
+            table.updateTableCookie(response);
+
+
+        });
+        response.error(function(){
+            makeWarn();
         });
     }
 }
 
-function addToTable(){
-    console.log("!!!!!!!!!!!!");
+function deleteCookie(name){
+    document.cookie = `${name}=""`
 }
+
 
 function isCorrectNumber(numberStr){
     if (!isNaN(numberStr)){
@@ -44,28 +100,35 @@ function isCorrectNumber(numberStr){
 
 }
 
-function isYValid(numberStr){
-    if (isCorrectNumber(numberStr)) {
-        return rangeY.indexOf(Number(numberStr)) !== -1;
+function isValidInput(x, y, r){
+    if ((x in rangeX)  && (r in rangeR) && (isYValid(y))){
+        return true;
     }
     else{
         return false;
     }
 }
 
-function makeWarn(input){
-    let form = document.getElementsByTagName("input")[0];
-    if (!isYValid(form.value)){
-        form.setAttribute("class", "invalid");
-        event.preventDefault();
+function isYValid(numberStr){
+    if (isCorrectNumber(numberStr)) {
+        return -3 <= (Number(numberStr)) <= 5;
     }
     else{
-        form.removeAttribute("class");
+        return false;
     }
 }
 
-function removeWarn(form){
-    if (form.hasAttribute("class")) {
-        form.removeAttribute("class");
+function fillTable(){
+    let records = decodeURIComponent(document.cookie).split("=").length;
+    let table = $("#result");
+    for (let i = 1; i < records; i++){
+        let record = decodeURIComponent(getCookie(`record${i}`));
+        record = $.parseHTML(record);
+        record = record[0];
+        table.append(record);
     }
 }
+
+
+// листенеры
+document.addEventListener("load", fillTable);
